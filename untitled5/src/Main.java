3f303f6aa1f764fe0e32074a10e9c027a8fafe40
Shamsa88 <139976530+Shamsa88@users.noreply.github.com>
2023-08-01 12:02:33 +0400

    Add files via upload

---
+++ untitled5/src/Main.java
@@ -0,0 +1,53 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        student s2 = new student("noor",12,"CS",3.15);
+
+        student S1 = new student();
+        S1.name = "noor";
+        S1.id = 151380;
+        S1.college = "sohar uni";
+        S1.gpa = 3.15;
+        S1.printname();
+        S1.printid();
+        S1.printcllege();
+        S1.printgpa();
+//        student S1= new student(
+    }
+}
+    class student {
+
+    student() {
+
+    }
+        student(String n , int i , String c,double g) {
+            name = n;
+            college = c;
+            id = i;
+            gpa = g;
+        }
+
+        String name;
+        int id;
+        String college;
+        double gpa;
+
+        void printname() {
+            System.out.println(name);
+        }
+
+        void printid() {
+            System.out.println(id);
+        }
+
+        void printcllege() {
+            System.out.println(college);
+        }
+
+        void printgpa() {
+            System.out.println(gpa);
+        }
+
+    }
